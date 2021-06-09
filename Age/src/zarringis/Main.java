51e5af5522c71581d72e3bd22e6e5c08782c3dd3
Karina Reed <zarringis@gmail.com>
2021-06-09 12:30:01 +0300

    Добавила программу Age

---
+++ Age/src/zarringis/Main.java
@@ -0,0 +1,18 @@
+package zarringis;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        int age = 33;
+        System.out.println(age);
+        int age1 = age + 2;
+        System.out.println(age1);
+        long fullAge = age;
+        System.out.println(fullAge);
+        int currentYear = 2021;
+        int myBirthYear = 1987;
+        byte myAge = (byte) (currentYear - myBirthYear);
+        System.out.println(myAge);
+    }
+}
