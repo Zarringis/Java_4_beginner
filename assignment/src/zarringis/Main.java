4dd2e2320b245ac42e60a62c099ac3a932c03187
Karina Reed <zarringis@gmail.com>
2021-06-09 13:18:33 +0300

    Добавила программу Assignment (про операции присвоения)

---
+++ assignment/src/zarringis/Main.java
@@ -0,0 +1,30 @@
+package zarringis;
+
+public class Main {
+
+    public static void main(String[] args) {
+	int a = 5;
+	a += 10;
+	System.out.println(a);
+	a -= 3;
+	System.out.println(a);
+	a *= 2;
+	System.out.println(a);
+	a /= 6;
+	System.out.println(a);
+	a <<= 4;
+	System.out.println(a);
+	a >>= 2;
+	System.out.println(a);
+	a ^= 2;
+	System.out.println(a);
+       /* int a = 5;
+        a += 10;        // 15
+        a -= 3;         // 12
+        a *= 2;         // 24
+        a /= 6;         // 4
+        a <<= 4;      // 64
+        a >>= 2;      // 16
+        System.out.println(a);  // 16*/
+    }
+}
