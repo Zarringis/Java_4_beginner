b830dd3505b0fd3be9e305298252817fc5d0770c
Karina Reed <zarringis@gmail.com>
2021-12-28 18:20:03 +0300

    Добавила изменения

--- assignment/src/zarringis/Main.java
+++ assignment/src/zarringis/Main.java
@@ -18,13 +18,7 @@ public class Main {
 	System.out.println(a);
 	a ^= 2;
 	System.out.println(a);
-       /* int a = 5;
-        a += 10;        // 15
-        a -= 3;         // 12
-        a *= 2;         // 24
-        a /= 6;         // 4
-        a <<= 4;      // 64
-        a >>= 2;      // 16
-        System.out.println(a);  // 16*/
+
+
     }
 }
